6b15a7905360acc84b75984cc43a6b88bbc9491b
rmattson <rmattson@gmail.com>
2024-01-18 15:21:24 -0800

    The last checkin didn't have the correct script. Unsure of why that was, but I think this one will do it.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def main():
-    text = get_text('./books/frankenstein.txt')
-    print(text)
+    book = './books/frankenstein.txt'
+    text = get_text(book)
     num_words = get_number_of_words(text)
-    print(f"Text contains {num_words} words")
+    chars = count_characters(text)
+    print(f"--- Begin report of {book} ---")
+    print(f"Text contains {num_words} words\n")
+    char_list = [[k, v] for k, v in chars.items() if k.isalpha()]
+    char_list.sort(key=lambda x: x[1], reverse=True)
+    for item in char_list:
+        print(f"The '{item[0]}' character was found {item[1]} times")
+    print(f"--- End report ---")
 
 def get_text(file_path):
     with open(file_path) as f:
@@ -11,6 +18,16 @@ def get_text(file_path):
 def get_number_of_words(str):
     words = str.split()
     return len(words)
+
+def count_characters(str):
+    chars = {}
+    for c in str:
+        c = c.lower()
+        if not c in chars:
+            chars[c] = 1
+        else:
+            chars[c] += 1
+    return chars
 
 if __name__ == '__main__':
     main()
